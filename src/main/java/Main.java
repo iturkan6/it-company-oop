51cb52bb9617bba820306677a7a0ad9296690eb6
iturkan6 <iturkan6@gmail.com>
2021-02-26 01:09:43 +0400

    Write a pattern for console menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,40 @@
+import controller.HrController;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        HrController hrController = new HrController();
+        boolean exit = true;
+        while (exit) {
+            System.out.println("..............Welcome to our application.............\n" +
+                    "              Please choose the action:\n" +
+                    "1. Come in\n                                3. Pass a test\n" +
+                    "2. Exit");
+            int number = scanner.nextInt();
+            if (number == 1) {
+                switch (hrController.toCheckAccess()) {
+                    case "hr":
+                        hrController.toSeeMessage();
+                        break;
+                    case "backend":
+                        break;
+                    case "frontend":
+                        break;
+                    case "director":
+                        break;
+                }
+            } else if (number == 3) {
+                hrController.toHire();
+            } else if (number == 2) {
+                hrController.toSave();
+                exit = false;
+            }
+
+
+        }
 
     }
 }
